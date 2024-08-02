18dfe92cc7ae68ab8f83ff6caf0572c238fc8431
satyam pandey <140078019+SatyamPandey810@users.noreply.github.com>
2024-08-02 18:59:40 +0530

    add

--- frontend/src/pages/admin/AllCategory-Subcategory.jsx
+++ frontend/src/pages/admin/AllCategory-Subcategory.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from 'react'
-import CategorySubCategoryUploads from './uploads/Category-SubCategory-Uploads'
+import CategoryUploads from './uploads/Category-Uploads'
+import SubCategoryUpload from './uploads/SubCategory-Upload'
 
 export default function AllCategorySubcategory() {
     const [openUploadCategory, setOpenUploadCategory] = useState(false)
+    const [openUploadSubCategory, setOpenUploadSubCategory] = useState(false)
 
     return (
         <>
-            <div>
+            <div className='row'>
                 {
                     openUploadCategory && (
-                        <CategorySubCategoryUploads onClose={() => setOpenUploadCategory(false)} />
+                        <CategoryUploads onClose={() => setOpenUploadCategory(false)} />
                     )
                 }
                 <div className='all-cu'>
@@ -18,6 +20,53 @@ export default function AllCategorySubcategory() {
                     </div>
                     <div className=''>
                         <button onClick={() => setOpenUploadCategory(true)} className='btn btn-warning'>Upload Courses</button>
+                    </div>
+                </div>
+                <div className=''>
+                    <table class="table table-bordered table-hover">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>First</th>
+                                <th>Last</th>
+                                <th>Handle</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                            </tr>
+                            <tr>
+                                <th>3</th>
+                                <td >Larry the Bird</td>
+                                <td>@twitter</td>
+                                <td>@twitter</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div className='row mt-4'>
+                {
+                    openUploadSubCategory && (
+                        <SubCategoryUpload onClose={() => setOpenUploadSubCategory(false)} />
+                    )
+                }
+                <div className='all-cu'>
+                    <div>
+                        <h1 className='text-light'>All Sub category</h1>
+                    </div>
+                    <div className=''>
+                        <button onClick={() => setOpenUploadSubCategory(true)} className='btn btn-warning'>Upload Courses</button>
                     </div>
                 </div>
                 <div className=''>
---
+++ frontend/src/pages/admin/uploads/Category-Uploads.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+
+export default function CategoryUploads({ onClose }) {
+    return (
+        <>
+            <div>
+                <div className="all-cu">
+                    <h2 className='text-light'>Upload categories</h2>
+                    <FontAwesomeIcon className='x-mark' icon={faXmark}
+                        onClick={onClose}
+                    />
+                </div>
+                <div className='container border mb-4'>
+                    <form className='mb-4'>
+
+                        <div className='row'>
+                            <div className="col-sm-6 mb-3">
+                                <label for="exampleInputEmail1" className="form-label">Category name</label>
+                                <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                            </div>
+                        </div>
+                        <div className='row d-flex justify-content-center'>
+                            <div className='col-sm-4'>
+                                <button className="btn btn-primary mt-3">Submit</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+
+        </>
+    )
+}
